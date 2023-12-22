93f51f1385d9b9c19ac2fc063e263c9d5e3199c6
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-12-22 19:51:39 +0530

    Refactor App component and add new features

--- src/App.js
+++ src/App.js
@@ -1,153 +1,149 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
+import axios from 'axios';
 
-const App = () => {
-  const [tickets, setTickets] = useState([]);
-  const [groupingOption, setGroupingOption] = useState('status');
-  const [sortOption, setSortOption] = useState('priority');
-  const [finalTickets, setFinalTickets] = useState([]);
+import './App.css';
 
-  // Fetch tickets data from the API
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers');
-        const data = await response.json();
-        setTickets(data.tickets);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData(); // Call the async function
+import List from './Components/List/List';
+import Navbar from './Components/Navbar/Navbar';
 
-  }, []); 
-  
-  const groupTickets = () => {
+function App() {
+  const statusList = ['In progress', 'Backlog', 'Todo', 'Done', 'Cancelled']
+  const userList = ['Anoop sharma', 'Yogesh', 'Shankar Kumar', 'Ramesh', 'Suresh']
+  const priorityList = [{name:'No priority', priority: 0}, {name:'Low', priority: 1}, {name:'Medium', priority: 2}, {name:'High', priority: 3}, {name:'Urgent', priority: 4}]
 
-    let groupedTickets = [];
+  const [groupValue, setgroupValue] = useState(getStateFromLocalStorage() || 'status')
+  const [orderValue, setorderValue] = useState('title')
+  const [ticketDetails, setticketDetails] = useState([]);
 
-    if (groupingOption === 'status') {
-      // Group by status
-      const statusGroups = {};
 
-      tickets.forEach((ticket) => {
-        const status = ticket.status;
-        if (!statusGroups[status]) {
-          statusGroups[status] = [];
+  const orderDataByValue = useCallback(async (cardsArry) => {
+    if (orderValue === 'priority') {
+      cardsArry.sort((a, b) => b.priority - a.priority);
+    } else if (orderValue === 'title') {
+      cardsArry.sort((a, b) => {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+
+        if (titleA < titleB) {
+          return -1;
+        } else if (titleA > titleB) {
+          return 1;
+        } else {
+          return 0;
         }
-        statusGroups[status].push(ticket);
       });
-
-      groupedTickets = Object.entries(statusGroups);
-    } else if (groupingOption === 'user') {
-      // Group by user
-      const userGroups = {};
-
-      tickets.forEach((ticket) => {
-        const user = ticket.userId;
-        if (!userGroups[user]) {
-          userGroups[user] = [];
-        }
-        userGroups[user].push(ticket);
-      });
-
-      groupedTickets = Object.entries(userGroups);
-    } else if (groupingOption === 'priority') {
-      // Group by priority
-      const priorityGroups = {};
-
-      tickets.forEach((ticket) => {
-        const priority = ticket.priority;
-        if (!priorityGroups[priority]) {
-          priorityGroups[priority] = [];
-        }
-        priorityGroups[priority].push(ticket);
-      });
-
-      groupedTickets = Object.entries(priorityGroups);
     }
+    await setticketDetails(cardsArry);
+  }, [orderValue, setticketDetails]);
 
-    setFinalTickets(groupedTickets);
-  };
+  function saveStateToLocalStorage(state) {
+    localStorage.setItem('groupValue', JSON.stringify(state));
+  }
 
-  // Function to dynamically sort tickets based on the selected option
-  const sortTickets = () => {
-    let sortedTickets = [...finalTickets];
-
-    if (sortOption === 'priority') {
-      // Sort by priority
-      sortedTickets.forEach((group) => {
-        group[1].sort((a, b) => b.priority - a.priority);
-      });
-    } else if (sortOption === 'title') {
-      // Sort by title
-      sortedTickets.sort((a, b) => a[1][0].title.localeCompare(b[1][0].title));
+  function getStateFromLocalStorage() {
+    const storedState = localStorage.getItem('groupValue');
+    if (storedState) {
+      return JSON.parse(storedState);
     }
-
-    setFinalTickets(sortedTickets);
-  };
-
-  useEffect(() => {
-    groupTickets(groupingOption);
-    sortTickets(sortOption);
-  }, [tickets]);
+    return null; 
+  }
 
   useEffect(() => {
-    console.log(finalTickets);
-  }, [finalTickets]);
-
-useEffect(() => {
-  groupTickets();
-}, [groupingOption]);
+    saveStateToLocalStorage(groupValue);
+    async function fetchData() {
+      const response = await axios.get('https://api.quicksell.co/v1/internal/frontend-assignment');
+      await refactorData(response);
   
-useEffect(() => {
-  sortTickets();
-}, [sortOption]);
+    }
+    fetchData();
+    async function refactorData(response){
+      let ticketArray = []
+        if(response.status  === 200){
+          for(let i=0; i<response.data.tickets.length; i++){
+            for(let j=0; j<response.data.users.length; j++){
+              if(response.data.tickets[i].userId === response.data.users[j].id){
+                let ticketJson = {...response.data.tickets[i], userObj: response.data.users[j]}
+                ticketArray.push(ticketJson)
+              }
+            }
+          }
+        }
+      await setticketDetails(ticketArray)
+      orderDataByValue(ticketArray)
+    }
+    
+  }, [orderDataByValue, groupValue])
 
+  function handleGroupValue(value){
+    setgroupValue(value);
+    console.log(value);
+  }
+
+  function handleOrderValue(value){
+    setorderValue(value);
+    console.log(value);
+  }
+  
   return (
-    <div>
-      <div>
-        {/* Controls for selecting grouping and sorting options */}
-        <label>
-          Group by:
-          <select
-            value={groupingOption}
-            onChange={(e) => setGroupingOption(e.target.value)}
-          >
-            <option value="status">Status</option>
-            <option value="user">User</option>
-            <option value="priority">Priority</option>
-          </select>
-        </label>
-
-        <label>
-          Sort by:
-          <select
-            value={sortOption}
-            onChange={(e) => setSortOption(e.target.value)}
-          >
-            <option value="priority">Priority</option>
-            <option value="title">Title</option>
-          </select>
-        </label>
-      </div>
-
-      <div>
-        {/* Display your Kanban board UI */}
-        {/* Example: */}
-        {finalTickets.map(([group, groupTickets]) => (
-          <div key={group}>
-            <p>{group}</p>
-            {groupTickets.map((ticket) => (
-              <div key={ticket.id}>
-                <p>{ticket.title}</p>
-                {/* Include other ticket information based on your UI design */}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      <Navbar
+        groupValue={groupValue}
+        orderValue={orderValue}
+        handleGroupValue={handleGroupValue}
+        handleOrderValue={handleOrderValue}
+      />
+      <section className="board-details">
+        <div className="board-details-list">
+          {
+            {
+              'status' : <>
+                {
+                  statusList.map((listItem) => {
+                    return(<List
+                      groupValue='status'
+                      orderValue={orderValue}
+                      listTitle={listItem}
+                      listIcon=''
+                      statusList={statusList}
+                      ticketDetails={ticketDetails}
+                    />)
+                  })
+                }
+              </>,
+              'user' : <>
+              {
+                userList.map((listItem) => {
+                  return(<List
+                    groupValue='user'
+                    orderValue={orderValue}
+                    listTitle={listItem}
+                    listIcon=''
+                    userList={userList}
+                    ticketDetails={ticketDetails}
+                  />)
+                })
+              }
+              </>,
+              'priority' : <>
+              {
+                priorityList.map((listItem) => {
+                  return(<List
+                    groupValue='priority'
+                    orderValue={orderValue}
+                    listTitle={listItem.priority}
+                    listIcon=''
+                    priorityList={priorityList}
+                    ticketDetails={ticketDetails}
+                  />)
+                })
+              }
+            </>
+            }[groupValue]
+          }
+        </div>
+      </section>
+    </>
   );
-};
+}
 
 export default App;
